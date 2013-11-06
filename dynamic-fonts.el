b26dd100f50d9c24f6aec0aadeaaf81864de80ae
Roland Walker <walker@pobox.com>
2013-11-06 10:06:40 -0500

    update/stdize copyrights

--- dynamic-fonts.el
+++ dynamic-fonts.el
@@ -1,6 +1,6 @@
 ;;; dynamic-fonts.el --- Set faces based on available fonts
 ;;
-;; Copyright (c) 2012 Roland Walker
+;; Copyright (c) 2012-13 Roland Walker
 ;;
 ;; Author: Roland Walker <walker@pobox.com>
 ;; Homepage: http://github.com/rolandwalker/dynamic-fonts
